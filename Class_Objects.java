effaebf35f1736a25a5c6a52c8603639e1c7c9b5
sohanraikar <sohanraikar021@gmail.com>
2025-03-21 09:15:36 +0530

    Class_Objects.java

--- Class_Objects.java
+++ Class_Objects.java
@@ -15,7 +15,7 @@ public void display() {
 	System.out.println("Std NAme:"+name+"Age:"+age);
 }
 public static void main(String[]args) {
-	Class_Objects s1=new Class_Objects("nokesh",20);
+	Class_Objects s1=new Class_Objects("sohan",20);
 	s1.display();
 }
 }
